dbdd05c60d0b8a3297c96be92337ce4f1cba1272
waveza4G <wavezaaia@gmail.com>
2024-07-17 11:15:15 +0700

    modified on template in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
-<template>
-  <router-view />
-</template>
+// <q-form ref="myForm">
 
+setup () {
+  const myForm = ref(null)
+
+  function validate () {
+    myForm.value.validate().then(success => {
+      if (success) {
+        // yay, models are correct
+      }
+      else {
+        // oh no, user has filled in
+        // at least one invalid value
+      }
+    })
+  }
+
+  // to reset validations:
+  function reset () {
+    myForm.value.resetValidation()
+  }
+
+  return {
+    myForm,
+    // ...
+  }
+}
 <script setup>
 defineOptions({
   name: 'App'
